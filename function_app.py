79d4e3cc12e9f30c949fd7270a5422eb67053c8a
J Furunes <jfu017@uit.no>
2025-04-30 15:25:04 +0200

    answers call

--- function_app.py
+++ function_app.py
@@ -4,47 +4,47 @@ import azure.communication.callautomation as azCall
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
+
 key_vault_name = "keyvault-t-bachelor2025"
 KVUri = f"https://{key_vault_name}.vault.azure.net"
 acs_secret_name = "communicationServicesBachelor"
+app_secret_name= "webhookOpptakApp"
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
 
 acs_retrieved_secret = client.get_secret(acs_secret_name)
+app_retrieved_secret=client.get_secret(app_secret_name)
 
 # Now you can use the API key:
 
 acsConnectionString = acs_retrieved_secret.value
 
+callback_uri=app_retrieved_secret
+
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.event_grid_trigger(arg_name="azeventgrid")
-def EventGridTriggerInboundCall(azeventgrid: func.EventGridEvent):
-    if func.EventGridEvent.event_type == "Microsoft.Communication.CallStarted":
-        logging.info('Python EventGrid trigger processed an event')
-        main()
+@app.function_name(name="eventGridTrigger")
+@app.event_grid_trigger(arg_name="event")
+def eventGridTest(event: func.EventGridEvent): 
+    logging.info('Python EventGrid trigger processed an event')
+    logging.info('Successfully reacted to call')
+    event_data=event.get_json()
+    # if event_data.get("eventType") == "Microsoft.Communication.IncomingCall":
+    call_connection=answerCall(event_data.get("incomingCallContext"))
+    logging.info(f'{event_data}')
+    logging.info(f'{call_connection.answered_by}')
+    # logging.info('Successfully answered call')
+    # recordCall(event_data, call_connection)
 
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Webhook triggered")
 
-def main():
+def answerCall(incomingCallContext):
+    logging.info("Ran function answerCall")
     try:
         call_automation_client = azCall.CallAutomationClient.from_connection_string(acsConnectionString)
-        response = call_automation_client.start_recording(call_locator=azCall.ServerCallLocator(azCall.server_call_id),
-            recording_content_type = azCall.RecordingContent.AUDIO,
-            recording_channel_type = azCall.RecordingChannel.UNMIXED,
-            recording_format_type = azCall.RecordingFormat.MP3,
-            recording_storage = azCall.AzureBlobContainerRecordingStorage(container_url="https://stfeilmelding001.blob.core.windows.net/opptaker"))
-        
-        # max_tones_to_collect = 5
-        # call_automation_client.get_call_connection(azCall.call_connection_id).start_recognizing_media( 
-        #     dtmf_max_tones_to_collect=max_tones_to_collect, 
-        #     input_type=azCall.RecognizeInputType.DTMF, 
-        #     target_participant=azCall.target_participant, 
-        #     initial_silence_timeout=30,  
-        #     dtmf_inter_tone_timeout=5, 
-        #     interrupt_prompt=True, 
-        #     choices=choices,
-        #     dtmf_stop_tones=[ azCall.DtmfTone.Pound ])
-        # logging.info("Start recognizing")
-
+        call_connection=call_automation_client.answer_call(incoming_call_context=incomingCallContext, callback_url=callback_uri)
+        logging.info(f'Answered call with ID: {call_connection.source_caller_id_number}')
+        return call_connection
     except Exception as e:
         logging.error(f"Error recieving call.")
